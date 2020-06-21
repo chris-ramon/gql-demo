f96ace448a2a9b0b3bba7708750ccae662a030e5
Chris Ramón <rchristian.ramon@gmail.com>
2020-06-21 00:53:51 -0500

    improves schema.sql formatting

--- schema.sql
+++ schema.sql
@@ -1,27 +1,27 @@
 -- INSERT INTO users (id, first_name, last_name) VALUES (1, 'gopher db 1', 'gopher db 2');
 -- INSERT INTO orders (id, user_id) VALUES (1, 1);
 -- INSERT INTO orders (id, user_id) VALUES (2, 1);
-
 CREATE TABLE users (
-  id 					integer NOT NULL,
-  first_name 	varchar(60) NOT NULL,
-  last_name 	varchar(60) NOT NULL,
-  hashed_password varchar(140)
-);
+	id INTEGER NOT NULL
+	,first_name VARCHAR(60) NOT NULL
+	,last_name VARCHAR(60) NOT NULL
+	,hashed_password VARCHAR(140)
+	);
 
 ALTER TABLE users ADD CONSTRAINT user_pkey PRIMARY KEY (id);
 
 CREATE TABLE orders (
-  id 					integer NOT NULL,
-  user_id integer NOT NULL
-);
+	id INTEGER NOT NULL
+	,user_id INTEGER NOT NULL
+	);
 
 ALTER TABLE orders ADD CONSTRAINT order_pkey PRIMARY KEY (id);
-ALTER TABLE orders ADD CONSTRAINT user_orders_fkey FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE orders ADD CONSTRAINT user_orders_fkey FOREIGN KEY (user_id) REFERENCES users (id);
 
 CREATE TABLE chats (
-  id    integer NOT NULL,
-  uuid 	varchar(55) NOT NULL
-);
+	id INTEGER NOT NULL
+	,uuid VARCHAR(55) NOT NULL
+	);
 
 ALTER TABLE chats ADD CONSTRAINT chat_pkey PRIMARY KEY (id);
