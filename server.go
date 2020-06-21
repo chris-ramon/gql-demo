3205dcc8bda99debfa676a82a3ae398de3cc4c65
Chris Ramón <rchristian.ramon@gmail.com>
2020-06-20 19:08:45 -0500

    use gqlgen handler

--- server.go
+++ server.go
@@ -3,17 +3,15 @@ package main
 import (
 	"context"
 	"database/sql"
-	"encoding/json"
-	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql"
-	"github.com/99designs/gqlgen/graphql/executor"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/volatiletech/sqlboiler/boil"
 	"github.com/chris-ramon/gql-demo/models"
+	"github.com/volatiletech/sqlboiler/boil"
 )
 
 type Resolver struct {
@@ -32,20 +30,20 @@ type queryResolver struct{ *Resolver }
 type userResolver struct{ *Resolver }
 
 func (qr *queryResolver) CurrentUser(ctx context.Context) (*models.User, error) {
-	user, err := models.FindUser(context.TODO(), qr.db, 1)
+	user, err := models.FindUser(context.Background(), qr.db, 1)
 	if err != nil {
-    log.Printf("failed to find user: %v", err)
-    return nil, nil
+		log.Printf("failed to find user: %v", err)
+		return nil, nil
 	}
-  return user, nil
+	return user, nil
 }
 
 func (ur *userResolver) Orders(ctx context.Context, obj *models.User) ([]*models.Order, error) {
-  orders, err := obj.Orders().All(ctx, ur.db)
-  if err != nil {
-    log.Printf("failed to find orders: %v", err)
-    return orders, nil
-  }
+	orders, err := obj.Orders().All(ctx, ur.db)
+	if err != nil {
+		log.Printf("failed to find orders: %v", err)
+		return orders, nil
+	}
 
 	return orders, nil
 }
@@ -68,55 +66,13 @@ func main() {
 
 	boil.DebugMode = true
 
-	http.Handle("/playground", playground.Handler("GraphQL playground", "/"))
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		r = r.WithContext(graphql.StartOperationTrace(r.Context()))
+	srv := handler.NewDefaultServer(NewExecutableSchema(NewSchemaConfig(db)))
 
-		schema := NewExecutableSchema(NewSchemaConfig(db))
-		exec := executor.New(schema)
-		exec.Use(extension.Introspection{})
+	http.Handle("/playground", playground.Handler("GraphQL playground", "/graphql"))
+	http.Handle("/graphql", srv)
 
-		var params graphql.RawParams
-		start := graphql.Now()
-
-		dec := json.NewDecoder(r.Body)
-		dec.UseNumber()
-
-		if err := dec.Decode(&params); err != nil {
-			log.Printf("failed to decode request body: %v", err)
-			http.Error(w, "failed 1", http.StatusBadRequest)
-			return
-		}
-
-		params.ReadTime = graphql.TraceTiming{
-			Start: start,
-			End:   graphql.Now(),
-		}
-
-		rc, err := exec.CreateOperationContext(r.Context(), &params)
-		if err != nil {
-			resp := exec.DispatchError(graphql.WithOperationContext(r.Context(), rc), err)
-			b, err := json.Marshal(resp)
-			if err != nil {
-				http.Error(w, "failed 2", http.StatusInternalServerError)
-				return
-			}
-			w.Write(b)
-			return
-		}
-
-		ctx := graphql.WithOperationContext(r.Context(), rc)
-		responses, ctx := exec.DispatchOperation(r.Context(), rc)
-
-		b, err2 := json.Marshal(responses(ctx))
-		if err2 != nil {
-			http.Error(w, "failed 3", http.StatusInternalServerError)
-			return
-		}
-
-		w.Write(b)
-	})
 	log.Println("server running on port :8080")
 	log.Println("graphql playground running on :8080/playground")
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
